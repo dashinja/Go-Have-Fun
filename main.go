a7d20cbfa0a146aa0ed060d3e5cb36e4e5483802
Byron <byron.miller@gmail.com>
2022-04-23 03:00:20 -0400

    Working basic functions for basic math

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+//TODO: https://go101.org/article/101.html is YOUR FRIEND
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	answer := Adder(1,2,3,4,5)
+	fmt.Print(answer)
+	inputs := []int {1,2,3,4,5}
+	complexAnswer := Operation(Adder, inputs...)
+	fmt.Println("complexAnswer: ", complexAnswer)
+	fmt.Println("wow answer: ", operationAdd5(1,2,3,4,5,6,7,8))
+	fmt.Println("wow answer dot dot dot: ", operationAdd5(inputs...))
+}
+
+func Operation(operationType func(...int) int , args ...int) (z int) {
+	z = operationType(args...)
+	return z
+}
+// func Operation(operationType func([]int) int, args ...int) (int z) {
+// 	z := operationType(args)
+// 	return z
+// }
+var operationAdd5 = func (args ...int) int {
+	sliceOf5 := []int {5}
+	appendedSlice := append(args, sliceOf5...)
+	return Operation(Adder, Adder(appendedSlice...))
+}
+
+func Adder(args ...int) int {
+	z := 0
+	for _, v := range args {
+		z += v
+	}
+	return z
+}
+
+func Subtract(args []int) int {
+	z := 0
+	for _, v := range args {
+		z -= v
+	}
+	return z
+}
+
+func multiply(args []int) int {
+	z := 0
+	for _, v := range args {
+		z *= v
+	}
+	return z
+}
+
+func divide(args []int) int {
+	z := 0
+	for _, v := range args {
+		z /= v
+	}
+	return z
+}
