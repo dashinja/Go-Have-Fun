773b8b862c19f2eea3d6bd643857f0b7d74d5da1
Byron <byron.miller@gmail.com>
2022-04-23 19:09:50 -0400

    Add test cases for utilities_test.go and remove main_test.go

--- utilities_test.go
+++ utilities_test.go
@@ -14,7 +14,11 @@ func TestReverseInts(t *testing.T) {
 		args args
 		want []int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "should reverse a slice of ints",
+			args: args{[]int{1, 2, 3, 4}},
+			want: []int{4, 3, 2, 1},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -26,19 +30,27 @@ func TestReverseInts(t *testing.T) {
 }
 
 func TestGetFunctionName(t *testing.T) {
+	// type args struct {
+	// 		func(int) int
+	// }
+
 	type args struct {
-		i interface{}
+		args func (int) int
 	}
 	tests := []struct {
 		name string
-		args args
+		args operationFuncMulti
 		want string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "should produce function name",
+			args: Adder,
+			want: "example.Adder",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GetFunctionName(tt.args.i); got != tt.want {
+			if got := GetFunctionName(tt.args); got != tt.want {
 				t.Errorf("GetFunctionName() = %v, want %v", got, tt.want)
 			}
 		})
